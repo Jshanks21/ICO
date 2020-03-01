b38747acf29c60f31addd8e75602be7400f2c844
Luis Egea <jshanks1001@gmail.com>
2020-03-01 11:31:09 -0800

    Imported ERC20Mintable, ERC20Pausable, and ERC20Detailed

--- contracts/OZToken.sol
+++ contracts/OZToken.sol
@@ -1,9 +1,10 @@
 pragma solidity >=0.4.21 <0.7.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract OZToken is ERC20, ERC20Detailed {
+contract OZToken is ERC20Mintable, ERC20Pausable, ERC20Detailed {
 
     constructor(string memory _name, string memory _symbol, uint8 _decimals)
         ERC20Detailed(_name, _symbol, _decimals)
