64c6445349a0ad63a46f15145fc2c01661f7546a
Luis Egea <jshanks1001@gmail.com>
2020-03-01 11:35:40 -0800

    Added crowdsale deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,14 @@
 const OZToken = artifacts.require("OZToken");
+const MyCrowdsale = artifacts.require("MyCrowdsale");
 
 module.exports = function (deployer) {
     let _name = "OZToken";
     let _symbol = "OZT";
     let _decimals = 18;
     deployer.deploy(OZToken, _name, _symbol, _decimals);
+    
+    // let _rate = '500';
+    // let _wallet = '*';
+    // let _token = OZToken.address;
+    // deployer.deploy(MyCrowdsale, _rate, _wallet, _token);
 };
