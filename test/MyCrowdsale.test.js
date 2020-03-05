2732bba1e6a9fb6134e1bcce74048fc05101f848
Luis Egea <jshanks1001@gmail.com>
2020-03-04 16:11:58 -0800

    Added testing for capped functionality

--- test/MyCrowdsale.test.js
+++ test/MyCrowdsale.test.js
@@ -35,12 +35,14 @@ describe('MyCrowdsale', function () {
 		// Crowdsale config
 		this.rate = '500';
 		this.wallet = wallet;
+		this.cap = ether('100');
 
 		// Deploy Crowdsale
 		this.crowdsale = await MyCrowdsale.new(
 			this.rate,
 			this.wallet,
-			this.token.address
+			this.token.address,
+			this.cap
 		);
 
 		// Gives crowdsale contract MinterRole access
@@ -50,6 +52,7 @@ describe('MyCrowdsale', function () {
 
 	describe('crowdsale', function () {
 
+		// Move to separate token test file
 		it('tracks the token name', async function () {
 			const name = await this.token.name();
 			name.should.equal(this.name);
@@ -81,13 +84,73 @@ describe('MyCrowdsale', function () {
 		});
 	});
 
+	describe('capped crowdsale', function () {
+
+		it('does not exceed the hard cap', async function () {
+			const cap = await this.crowdsale.cap();
+			cap.should.be.bignumber.equal(this.cap);
+		});
+
+		it('rejects transactions below the minimum cap', async function () {
+			await expectRevert(
+				this.crowdsale.buyTokens(investor2, { value: ether('0.001'), from: investor2 }),
+				'Investor cap reached: Min amount is 0.002 Ether. Max amount is 50 Ether.'
+			)
+		});
+
+		it('rejects transactions above the maximum cap', async function () {
+			await expectRevert(
+				this.crowdsale.buyTokens(investor2, { value: ether('51'), from: investor2 }),
+				'Investor cap reached: Min amount is 0.002 Ether. Max amount is 50 Ether.'
+			)
+		});
+
+	});
+
 	describe('accepting payments', function () {
 
-		it('accept payments', async function () {
+		it('accepts payments', async function () {
 			const value = ether('1');
 			const purchaser = investor2;
 			await this.crowdsale.sendTransaction({ value: value, from: investor1 }).should.be.fulfilled;
 			await this.crowdsale.buyTokens(investor1, { value: value, from: purchaser }).should.be.fulfilled;
+		});
+	});
+
+	describe('token purchases', function () {
+
+		describe('when the investor has already met the minimum cap', function () {
+
+			it('allows the investor to contribute below the minimum cap', async function () {
+				// Minimum contribution met
+				const minCapReached = ether('1');
+				await this.crowdsale.buyTokens(investor2, { value: minCapReached, from: investor2 });
+				// Following contributions under minimum cap should be accepted
+				const belowMinCap = 1; // 1 wei
+				await this.crowdsale.buyTokens(investor2, { value: belowMinCap, from: investor2 }).should.be.fulfilled;
+			});
+		});
+
+		describe('when the total contributions exceeds the investor maximum cap', function () {
+
+			it('rejects the transaction', async function () {
+				// Maximum contribution met
+				const maxContribution = ether('50');
+				await this.crowdsale.buyTokens(investor1, { value: maxContribution, from: investor1 });
+				// All following contributions should be rejected
+				const smallestContribution = 1; // 1 wei
+				await this.crowdsale.buyTokens(investor1, { value: smallestContribution, from: investor1 }).should.be.rejectedWith('revert');
+			});
+		});
+
+		describe('when the contribution is within the valid range', function () {
+			const value = ether('2');
+
+			it('succeeds and updates the contribution amount', async function () {
+				await this.crowdsale.buyTokens(investor2, { value: value, from: investor2 }).should.be.fulfilled;
+				const contribution = await this.crowdsale.getContributionAmount({ from: investor2 });
+				contribution.should.be.bignumber.equal(value);
+			});
 		});
 	});
 
