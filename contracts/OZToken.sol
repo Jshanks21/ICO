d1c6bc78b1b05ebc0e9992c487ad57e933247290
Luis Egea <jshanks1001@gmail.com>
2020-03-15 13:21:16 -0700

    Added Initial Supply

--- contracts/OZToken.sol
+++ contracts/OZToken.sol
@@ -1,15 +1,24 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract OZToken is ERC20Mintable, ERC20Pausable, ERC20Detailed {
+/**
+ * @title OZToken
+ * @notice This token was created for use in MyCrowdsale.sol
+ * @dev This is a basic ERC-20 implementation using the OpenZeppelin library for base and extended functions
+ */
+contract OZToken is ERC20Mintable, ERC20Detailed {
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals)
-        ERC20Detailed(_name, _symbol, _decimals)
-        public
-    {
-
-    }
+	/**
+     * @dev Sets the values for `name`, `symbol`, and `decimals`. All three of
+     * these values are immutable: they can only be set once during
+     * construction. See {ERC20Detailed-construcotor}.
+     */
+	constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _initialSupply)
+		ERC20Detailed(_name, _symbol, _decimals)
+		public
+	{
+		_mint(msg.sender, _initialSupply);
+	}
 }
