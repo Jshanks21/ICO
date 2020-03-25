49af49720c190b636510dea77966ca7391eda55b
Luis Egea <jshanks1001@gmail.com>
2020-03-25 13:52:44 -0700

    Basic Test Token

---
+++ contracts/TestERC20.sol
@@ -0,0 +1,7 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestERC20 is IERC20, ERC20 {
+}
